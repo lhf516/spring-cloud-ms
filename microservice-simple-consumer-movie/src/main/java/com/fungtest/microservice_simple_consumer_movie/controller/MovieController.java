7e80275f782c089d2504bf61140b64bd4cc9a599
Lam Hoi Fung <lhf516@gmail.com>
2024-10-10 11:51:06 +0800

    Added self-defined variables to application.yml

--- microservice-simple-consumer-movie/src/main/java/com/fungtest/microservice_simple_consumer_movie/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/fungtest/microservice_simple_consumer_movie/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.fungtest.microservice_simple_consumer_movie.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,9 +17,12 @@ public class MovieController {
   @Autowired
   private RestTemplate restTemplate;
 
+  @Value("${user.userServiceUrl}")
+  private String userServiceUrl;
+
   @GetMapping("/{id}")
   public User findById(@PathVariable Long id) {
-    return this.restTemplate.getForObject("http://localhost:8000/api/v1/users/" + id, User.class);
+    return this.restTemplate.getForObject(this.userServiceUrl + id, User.class);
   }
 
 }
