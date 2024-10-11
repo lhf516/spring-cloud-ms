47443c1fe7b2e15aa8ddfc6fd4505909bb3e7b07
Lam Hoi Fung <lhf516@gmail.com>
2024-10-11 22:57:32 +0800

    Copied eureka to add authenticating

---
+++ microservice-discovery-eureka-authenticating/src/test/java/com/fungtest/microservice_discovery_eureka/MicroserviceDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fungtest.microservice_discovery_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDiscoveryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
